9508bd54dcaecccc0fe5fa25b0de84c5b7c0d5c7
William Yu <wy427@nyu.edu>
2022-03-13 20:06:19 -0400

    completed

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,24 @@
 # Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+    words =sentence.split
+    reversed_words = words.map do |word|
+        word.reverse
+    end
+
+    reversed_words.join(" ")
+end
+
+
+# def reverse_each_word(sentence)
+#     words = sentence.split
+  
+#     reversed_words = words.map(&:reverse)
+  
+#     reversed_words.join(" ")
+#   end
+
+# def reverse_each_word(sentence)
+#     sentence.split.map(&:reverse).join(" ")
+#   end
